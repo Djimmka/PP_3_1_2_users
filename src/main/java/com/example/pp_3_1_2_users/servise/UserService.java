2dc843b0504c24c9ad520ffeba276409fbdf522f
Djimmka <dimsti94@gmail.com>
2022-11-25 19:19:58 +0300

    3_1_2 готова

--- src/main/java/com/example/pp_3_1_2_users/servise/UserService.java
+++ src/main/java/com/example/pp_3_1_2_users/servise/UserService.java
@@ -1,56 +1,14 @@
 package com.example.pp_3_1_2_users.servise;
 
 import com.example.pp_3_1_2_users.models.User;
-import com.example.pp_3_1_2_users.repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService {
-
-    private final UsersRepository usersRepository;
-
-    @Autowired
-    public UserService(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
-
-    public List<User> findAll() {
-        return usersRepository.findAll();
-    }
-
-    public User findById(long id) {
-        Optional<User> user = usersRepository.findById(id);
-        return user.orElse(null);
-    }
-
-    @Transactional
-    public void save(User user) {
-        usersRepository.save(user);
-    }
-
-    @Transactional
-    public void update(long id, User updUser) {
-        updUser.setId(id);
-        usersRepository.save(updUser);
-    }
-
-    @Transactional
-    public void delete(long id) {
-        usersRepository.deleteById(id);
-    }
-
-    @Transactional
-    public void gen5Users() {
-        for (int i = 1; i < 6; i++) {
-            User user = new User(2220 + i, "lastName_" + i, "name_" + i);
-            update((long) i, user);
-        }
-    }
-
+public interface UserService {
+    public List<User> findAll();
+    public User findById(long id);
+    public void save(User user);
+    public void update(long id, User updUser);
+    public void delete(long id);
+    public void gen5Users();
 }
